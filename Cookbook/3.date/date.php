0fd8e3f1747fc29be89e1d6f6641722cbcb7f914
SU02aiggshon <wly1582634453@163.com>
2017-04-02 20:17:33 +0800

    更新 date.php

--- Cookbook/3.date/date.php
+++ Cookbook/3.date/date.php
@@ -160,4 +160,183 @@ print $elapsed . "\n";
 list($microseconds, $second) = explode(' ', microtime());
 print $second . $microseconds . getmygid() . "\n";
 
+// 3-26 使用非公历日历
+$jd = gregoriantojd(3,9,1876);
+$gregorian = cal_from_jd($jd, CAL_GREGORIAN);
+print_r($gregorian);
+
+// 3-27 使用儒略历
+$jd = cal_to_jd(CAL_JULIAN, 2, 29, 1900);
+$julian = cal_from_jd($jd, CAL_JULIAN);
+print_r($julian);
+$gregorian = cal_from_jd($jd, CAL_GREGORIAN);
+print_r($gregorian);
+
+// 3-28 使用法国共和历
+$jd = cal_to_jd(CAL_FRENCH, 8, 13, 11);
+$french = cal_from_jd($jd, CAL_FRENCH);
+print_r($french);
+$gregorian = cal_from_jd($jd, CAL_GREGORIAN);
+print_r($gregorian);
+
+// 3-29 使用犹太历
+$jd = cal_to_jd(CAL_JEWISH, 3, 25, 5774);
+$jewish = cal_from_jd($jd, CAL_JEWISH);
+print_r($jewish);
+$gregorian = cal_from_jd($jd, CAL_GREGORIAN);
+print_r($gregorian);
+
+// 3-30 日历
+$month = isset($_GET['month']) ? intval($_GET['month']) : date('m');
+$year = isset($_GET['year']) ? intval($_GET['year']) : date('Y');
+$cal = new LittleCalendar($month, $year);
+print $cal->html();
+
+?>
+
+<style type="text/css">
+    .prev { text-align: left; }
+    .next { text-align: right; }
+    .day, .month, .weekday { text-align: center; }
+    .today { background: yellow; }
+    .blank {  }
+</style>
+
+<?php
+
+class LittleCalendar{
+    /** DateTime */
+    protected $monthToUse;
+    protected $prepared = false;
+    protected $days = [];
+
+    public function __construct($month, $year){
+        /** 为要显示的月份建立一个 DateTime */
+        $this->monthToUse = DateTime::createFromFormat('Y-m|', sprintf("%04d-%02d", $year, $month));
+        $this->prepare();
+    }
+
+    protected function prepare(){
+        // 建立一个数组， 包含一个月每天的信息
+        // 会适当地填充
+        // 开头和末尾
+        // 首先， 第一行显示星期几
+        foreach (['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'] as $dow){
+            $endOfRow = ($dow == 'Sa');
+            $this->days[] = ['type' => 'dow', 'label' => $dow, 'endOfRow' => $endOfRow];
+        }
+
+        // 接下来， 在一周的第一天之前放置占位符
+        for ($i = 0, $j = $this->monthToUse->format('w'); $i < $j; $i++){
+            $this->days[] = ['type' => 'blank'];
+        }
+
+        // 然后， 对于这个月的每一天分别有一项
+        $today = date('Y-m-d');
+        $days = new DatePeriod($this->monthToUse, new DateInterval('P1D'), $this->monthToUse->format('t') - 1);
+        foreach ($days as $day){
+            $isToday = ($day->format('Y-m-d') == $today);
+            $endOfRow = ($day->format('w') == 6);
+            $this->days[] = ['type' => 'day', 'label' => $day->format('j'), 'today' => $isToday, 'endOfRow' => $endOfRow];
+        }
+
+        // 最后， 如果 endOfWeek 不是这个月最后一天
+        // 在末尾放置占位符
+        if (! $endOfRow){
+            for ($i = 0, $j = 6 - $day->format('w'); $i < $j; $i++){
+                $this->days[] = ['type' => 'blank'];
+            }
+        }
+    }
+
+    public function html($opts = [])
+    {
+        if(! isset($opts['id'])){
+            $opts['id'] = 'calendar';
+        }
+        if(! isset($opts['month_link'])){
+            $opts['month_link'] = '<a href="'.htmlentities($_SERVER['PHP_SELF']).'?'.'month=%d&amp;year=%d>%s</a>';
+        }
+        $classes = [];
+        foreach (['prev', 'month', 'next', 'weekday', 'blank', 'day', 'today'] as $class){
+            if(isset($opts['class']) && isset($opts['class'][$class])){
+                $classes[$class] = $opts['class'][$class];
+            }else{
+                $classes[$class] = $class;
+            }
+        }
+
+        /** 为上个月建立一个DateTime */
+        $prevMonth = clone $this->monthToUse;
+        $prevMonth->modify("-1 month");
+        $prevMonthLink = sprintf($opts['month_link'], $prevMonth->format('m'), $prevMonth->format('Y'), '&laquo;');
+
+        /** 为下个月建立一个DateTime */
+        $nexeMonth = clone $this->monthToUse;
+        $nexeMonth->modify("+1 month");
+        $nexeMonthLink = sprintf($opts['month_link'], $nexeMonth->format('m'), $nexeMonth->format('Y'), '&raquo;');
+
+        $html = '<table id="'.htmlentities($opts['id']).'"><tr>
+            <td class="'.htmlentities($classes['prev']).'">'.$prevMonthLink.'</td>
+            <td class="'.htmlentities($classes['month']).'" colspan="5">'.$this->monthToUse->format('F Y').'</td>
+            <td class="'.htmlentities($classes['next']).'">'.$nexeMonthLink.'</td>
+        </tr>';
+        $html .= '<tr>';
+
+        $lastDayIndex = count($this->days) - 1;
+        foreach ($this->days as $i => $day){
+            switch ($day['type']) {
+                case 'dow':
+                    $class = 'weekday';
+                    $label = htmlentities($day['label']);
+                    break;
+                case 'blank':
+                    $class = 'blank';
+                    $label = '&nbsp;';
+                    break;
+                case 'day':
+                    $class = $day['today'] ? 'today' : 'day';
+                    $label = htmlentities($day['label']);
+                    break;
+            }
+                $html .= '<td class="'.htmlentities($classes[$class]).'">'.$label.'</td>';
+
+            if(isset($day['endOfRow']) && $day['endOfRow']) {
+                $html .= "</tr>\n";
+                if ($i != $lastDayIndex) {
+                    $html .= '<tr>';
+                }
+            }
+        }
+        $html .= '</table>';
+        return $html;
+    }
+
+    public function text()
+    {
+        $lineLength = strlen('Su Mo Tu We Th Fr Sa');
+        $header = $this->monthToUse->format('F Y');
+        $headerSpacing = floor($lineLength - strlen($header))/2;
+
+        $text = str_repeat(' ', $headerSpacing).$header."\n";
+        foreach ($this->days as $i => $day){
+            switch ($day['type']){
+                case 'dow':
+                    $text .= sprintf('% 2s', $day['label']);
+                    break;
+                case 'blank':
+                    $text .= ' ';
+                    break;
+                case 'day':
+                    $text .= sprintf('% 2d', $day['label']);
+                    break;
+            }
+            $text .= (isset($day['endOfRow']) && $day['endOfRow']) ? "\n" : " ";
+        }
+        if($text[strlen($text) - 1] != "\n"){
+            $text .= "\n";
+        }
+        return $text;
+    }
+}
 
